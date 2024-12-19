97c628908e77abc7a61f94253592fc3b4c79cfb1
Kaivv1 <kvalchevv1@gmail.com>
2024-12-19 17:25:56 +0200

    adding files working fine

--- addFiles.go
+++ addFiles.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"image/color"
+	"io"
 	"log"
+	"os"
+	"path/filepath"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
@@ -14,31 +17,16 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-// type FileToAdd struct {
-// 	DBT string
-// 	RequestType string
-// 	Name string
-// 	File
-// }
+//	type FileToAdd struct {
+//		DBT string
+//		RequestType string
+//		Name string
+//		File
+//	}
 
-func AddBorder(w fyne.CanvasObject, borderColor color.Color, borderWidth float32) *fyne.Container {
-	topBorder := canvas.NewRectangle(borderColor)
-
-	bottomBorder := canvas.NewRectangle(borderColor)
-
-	leftBorder := canvas.NewRectangle(borderColor)
-
-	rightBorder := canvas.NewRectangle(borderColor)
-
-	return container.NewBorder(
-		topBorder,
-		bottomBorder,
-		leftBorder,
-		rightBorder,
-		w,
-	)
-}
 func makeAddFilesSpace(storage *Storage, w fyne.Window) (fyne.CanvasObject, func()) {
+	var chosenFilePath string
+
 	title := canvas.NewText("Добави заявки", color.White)
 	title.TextStyle = fyne.TextStyle{Bold: true}
 	title.Alignment = fyne.TextAlignCenter
@@ -79,7 +67,9 @@ func makeAddFilesSpace(storage *Storage, w fyne.Window) (fyne.CanvasObject, func
 	input := widget.NewEntry()
 	input.PlaceHolder = "Име"
 
-	chooseFileButton := widget.NewButton("Избери файл", func() {
+	chooseFileButton := &widget.Button{}
+
+	chooseFileButton = widget.NewButton("Избери файл", func() {
 		d := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
 			if err != nil {
 				dialog.ShowError(err, w)
@@ -91,28 +81,78 @@ func makeAddFilesSpace(storage *Storage, w fyne.Window) (fyne.CanvasObject, func
 			defer reader.Close()
 
 			filePath := reader.URI().Path()
-			log.Printf("Selected file: %s", filePath)
+			chooseFileButton.SetText("Има избран файл")
+
+			chosenFilePath = filePath
+			w.Content().Refresh()
 		}, w)
 		d.Resize(fyne.NewSize(950, 800))
 		d.Show()
+		d.SetOnClosed(func() {
+			chooseFileButton.SetText("Избери файл")
+			w.Content().Refresh()
+		})
+		w.Content().Refresh()
 	})
 
 	platformSelector := widget.NewSelect(PLATFORMS[:], func(s string) {
-
 	})
-
 	platformSelector.PlaceHolder = "Избери платформа"
-	addFileToFolderBtn := widget.NewButton("Добави файла към папката", func() {
-		log.Printf("DBT: %s", DBTSelector.Selected)
-		log.Printf("Request type: %s", platformSelector.Selected)
-		log.Printf("Input: %s", input.Text)
-	})
-	resetButton := widget.NewButton("Ресетни", func() {})
 
 	actionSelector := widget.NewSelect(REQUEST_TYPES[:], func(s string) {
 
 	})
 	actionSelector.PlaceHolder = "Вид заявка"
+
+	resetButton := widget.NewButton("Ресетни", func() {
+		chosenFilePath = ""
+		platformSelector.Selected = ""
+		actionSelector.Selected = ""
+		DBTSelector.Selected = ""
+		input.Text = ""
+		chooseFileButton.SetText("Избери файл")
+
+		DBTSelector.Refresh()
+		actionSelector.Refresh()
+		platformSelector.Refresh()
+		input.Refresh()
+	})
+
+	addFileToFolderBtn := widget.NewButton("Добави файла към папката", func() {
+		desktopPath, err := getDesktopPath()
+		if err != nil {
+			log.Println("Error getting error path when adding file to folder")
+		}
+		extension := filepath.Ext(chosenFilePath)
+
+		newFilePath := filepath.Join(desktopPath, "ДБТ", DBTSelector.Selected, platformSelector.Selected, actionSelector.Selected, fmt.Sprintf("%s%s", input.Text, extension))
+
+		chosenFile, err := os.Open(chosenFilePath)
+		if err != nil {
+			dialog.ShowError(err, w)
+			return
+		}
+		defer chosenFile.Close()
+
+		newFile, err := os.Create(newFilePath)
+		if err != nil {
+			dialog.ShowError(err, w)
+			return
+		}
+		defer newFile.Close()
+
+		_, err = io.Copy(newFile, chosenFile)
+		if err != nil {
+			dialog.ShowError(err, w)
+			return
+		}
+		chooseFileButton.SetText("Избери файл")
+		dialog.ShowInformation("Нотификация", "Файлът е добавен към папката", w)
+
+		log.Printf("File path: %s\n", newFilePath)
+		log.Printf("File: %s\n", filepath.Base(chosenFilePath))
+
+	})
 	grid := container.NewVBox(container.NewBorder(
 		nil, nil, chooseFileButton, container.NewVBox(actionSelector, resetButton, addFileToFolderBtn), container.NewVBox(DBTSelector, platformSelector, input)))
 
